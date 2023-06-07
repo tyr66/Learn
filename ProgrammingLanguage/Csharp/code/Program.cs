4e3fe3b9699b707a1525debad998c06512998a62
tyr66 <1847009824tan@gmail.com>
2023-06-07 12:09:48 +0800

    update:Program language

---
+++ ProgrammingLanguage/Csharp/code/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Reflection;
+using System;
+
+using ReflectAndAttribute;
+
+namespace Learn
+{
+    public class Base:ITest{
+        protected int baseInt;
+        public static int staticInt;
+    }
+
+    public interface ITest
+    {
+        void show(){}
+    }
+
+    public class Derived:Base
+    {
+        public class NestedClass{
+            public int id;
+        }
+        protected int derivedInt;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReflectAndAttribute.GenericInstantiate.Test();
+            Console.ReadKey();
+        }
+
+       
+    }
+
+}
